caae5ed6f112aab2b218a50af206e33da7f74561
jungooji <zzzz465@naver.com>
2024-04-16 20:11:06 +0900

    commit

---
+++ infra/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "google-beta" {
+  user_project_override = true
+}
+
+provider "google-beta" {
+  alias = "no_user_project_override"
+  user_project_override = false
+}

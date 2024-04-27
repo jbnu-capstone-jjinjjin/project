eb467d6df0e137eb04e18d916f8781bc6aadc2c4
iju42829 <ijud4282@gmail.com>
2024-04-27 13:39:50 +0900

    Setting Spring Security Config

---
+++ backend/src/main/java/com/jbnu/capstone/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.jbnu.capstone.config;
+
+import com.jbnu.capstone.filter.FirebaseAuthenticationTokenFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()  // CSRF 보호 기능을 비활성화
+
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/","/swagger-ui/**", "/v3/api-docs/**").permitAll() // Swagger 제외
+                        .anyRequest().authenticated())
+
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .addFilterBefore(new FirebaseAuthenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}

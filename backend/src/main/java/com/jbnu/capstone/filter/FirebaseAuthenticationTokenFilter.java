9a08038a3254f52473a8af6f52feb8fa133e5695
iju42829 <ijud4282@gmail.com>
2024-04-27 13:36:42 +0900

    Add Firebase Filter

---
+++ backend/src/main/java/com/jbnu/capstone/filter/FirebaseAuthenticationTokenFilter.java
@@ -0,0 +1,69 @@
+package com.jbnu.capstone.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+import com.jbnu.capstone.dto.ErrorDTO;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+@Slf4j
+public class FirebaseAuthenticationTokenFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    // Swagger UI 및 API 문서 관련 경로에 대해 필터를 적용하지 않음
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String path = request.getRequestURI();
+
+        return path.startsWith("/swagger-resources/") || path.startsWith("/swagger-ui/")
+                || path.startsWith("/v3/api-docs") || path.startsWith("/webjars/");
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authToken = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(authToken)) {
+            try {
+
+                FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(authToken);
+                String uid = decodedToken.getUid();
+
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(uid, null, new ArrayList<>());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            } catch (FirebaseAuthException e) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.setContentType("application/json");
+                response.getWriter().write(objectMapper
+                        .writeValueAsString(new ErrorDTO(HttpServletResponse.SC_UNAUTHORIZED,
+                        "Invalid token")));
+                return;
+            }
+
+        } else {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.getWriter().write(objectMapper
+                    .writeValueAsString(new ErrorDTO(HttpServletResponse.SC_UNAUTHORIZED,
+                            "Authorization token is missing")));
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}

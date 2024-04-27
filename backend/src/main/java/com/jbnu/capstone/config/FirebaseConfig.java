61ccf8c339fe5ccd2459a7cb01f1ae62ac699b0c
iju42829 <ijud4282@gmail.com>
2024-04-27 13:28:47 +0900

    Init Firebase Config

---
+++ backend/src/main/java/com/jbnu/capstone/config/FirebaseConfig.java
@@ -0,0 +1,35 @@
+package com.jbnu.capstone.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Value("${firebase.config-path}")
+    private String configPath;
+
+    @Bean
+    public FirebaseApp initializeFirebaseApp() throws IOException {
+        InputStream serviceAccount = new ClassPathResource
+                (configPath).getInputStream();
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            return FirebaseApp.initializeApp(options);
+        } else {
+            return FirebaseApp.getInstance();
+        }
+    }
+}

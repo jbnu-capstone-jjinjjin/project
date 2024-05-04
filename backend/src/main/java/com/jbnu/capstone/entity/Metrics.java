cf8f0e8f69de93982dde4394cd5755f513fb0a01
iju42829 <ijud4282@gmail.com>
2024-05-04 20:02:21 +0900

    Create Machine and Metrics entity.

---
+++ backend/src/main/java/com/jbnu/capstone/entity/Metrics.java
@@ -0,0 +1,34 @@
+package com.jbnu.capstone.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.Type;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Metrics {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "machine_id")
+    private Machine machine;
+
+    private LocalDateTime createdAt;
+
+    private String metricType;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    private Map<String, Object> data;
+}

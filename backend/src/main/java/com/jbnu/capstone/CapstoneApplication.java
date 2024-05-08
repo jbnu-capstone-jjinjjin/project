332c0dfad0a853d3532ef56655ff70654b3b0679
iju42829 <116072376+iju42829@users.noreply.github.com>
2024-05-08 13:12:14 +0900

    Fix/review (#8)

--- backend/src/main/java/com/jbnu/capstone/CapstoneApplication.java
+++ backend/src/main/java/com/jbnu/capstone/CapstoneApplication.java
@@ -2,8 +2,9 @@ package com.jbnu.capstone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class CapstoneApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/com/jbnu/capstone/config/FirebaseConfig.java
+++ backend/src/main/java/com/jbnu/capstone/config/FirebaseConfig.java
@@ -11,7 +11,7 @@ import org.springframework.core.io.ClassPathResource;
 import java.io.IOException;
 import java.io.InputStream;
 
-@Configuration
+// @Configuration
 public class FirebaseConfig {
 
     @Value("${firebase.config-path}")

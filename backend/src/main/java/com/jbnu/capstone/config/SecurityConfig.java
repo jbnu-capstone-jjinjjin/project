f68dc1fb0214d993254895ece1efe1a9e9476728
iju42829 <ijud4282@gmail.com>
2024-05-08 12:49:14 +0900

    disable spring security

--- backend/src/main/java/com/jbnu/capstone/config/SecurityConfig.java
+++ backend/src/main/java/com/jbnu/capstone/config/SecurityConfig.java
@@ -11,8 +11,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class SecurityConfig implements WebMvcConfigurer {
 
     @Override

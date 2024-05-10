4d58ec291b0394ac9c0861b7b62eb88638181883
iju42829 <ijud4282@gmail.com>
2024-05-10 17:35:43 +0900

    updateMetrics 제거

--- backend/src/main/java/com/jbnu/capstone/controller/MetricsController.java
+++ backend/src/main/java/com/jbnu/capstone/controller/MetricsController.java
@@ -40,21 +40,4 @@ public class MetricsController {
         return new ResponseDTO(HttpStatus.OK.value(), "메타 데이터를 성공적으로 추가했습니다.");
     }
 
-    @PatchMapping("/{id}")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "성공",
-                    content = @Content(schema = @Schema(implementation = ResponseDTO.class))),
-            @ApiResponse(responseCode = "500", description = "실패",
-                    content = @Content(schema = @Schema(implementation = ResponseDTO.class)))
-    })
-    @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "특정 id의 메타 데이터를 수정합니다.")
-    public ResponseDTO updateMetrics(@PathVariable("id") Long metricsId,
-                                @RequestBody RequestUpdateMetricsDTO requestUpdateMetricsDTO) {
-
-        metricsService.updateMetrics(metricsId, requestUpdateMetricsDTO);
-
-        return new ResponseDTO(HttpStatus.OK.value(), "특정 id의 메타 데이터를 성공적으로 수정했습니다.");
-    }
 }
-

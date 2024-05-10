082b785a7c9be85f7dc588102fa4c6c79c7e0b04
DeokJong <129056857+DeokJong@users.noreply.github.com>
2024-05-10 09:12:35 +0900

    Feature/collector (#9)
    jsx을 tsx로 변경함.
    then catch문을 async await으로 변경함.

---
+++ collector/src/components/LoginPage.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { signInWithPopup, GoogleAuthProvider, UserCredential, AuthError } from 'firebase/auth';
+import { auth } from '../firebaseConfig';
+
+const LoginPage= ()=> {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>('');
+
+  const handleLogin = () => {
+    setLoading(true);
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result: UserCredential) => {
+        console.log("Login Successful:", result.user);
+        navigate('/');
+      })
+      .catch((error: AuthError) => {
+        console.error("Login Failed:", error);
+        setErrorMessage("Failed to login. Please try again.");
+        setLoading(false);
+      });
+  };
+
+  return (
+    <header id='header'>
+      <div>
+        <h1>Hello world!</h1>
+        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+        <button
+          onClick={handleLogin}
+          disabled={loading}
+          aria-label="Login with Google"
+        >
+          {loading ? "Logging in..." : "Login with Google"}
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default LoginPage;

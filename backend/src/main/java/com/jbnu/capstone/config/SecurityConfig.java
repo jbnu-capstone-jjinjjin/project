fcb14c02c4c0ae475d3b1157d3fc3c53f209e499
iju42829 <ijud4282@gmail.com>
2024-05-12 20:36:07 +0900

    Cors 문제 해결

--- backend/src/main/java/com/jbnu/capstone/config/SecurityConfig.java
+++ backend/src/main/java/com/jbnu/capstone/config/SecurityConfig.java
@@ -15,15 +15,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //@EnableWebSecurity
 public class SecurityConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // React 애플리케이션이 실행되는 주소
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http

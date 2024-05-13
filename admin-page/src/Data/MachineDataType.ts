b4e591a16a39f57963ee9a59e46fd2d9c9890137
KIMJAEHYEON <117272556+KimJH0709@users.noreply.github.com>
2024-05-14 00:55:13 +0900

    Feature/admin page (#16)
    eslint에 맞춰서 코드 수정하고
    jsx -> tsx로 변환 중 오류 나는 부분 수정

---
+++ admin-page/src/Data/MachineDataType.ts
@@ -0,0 +1,52 @@
+export interface Machine {
+  machine_id: number
+  machineName: string
+}
+
+export interface MachineData {
+  status: number
+  timestamp: string
+  message: string
+  data: Machine[]
+}
+
+export interface ErrorResponse {
+  message: string
+}
+
+export interface MachineListProps {
+  onViewDetails: (machineId: number) => void
+}
+
+export interface MachineDetail {
+  metrics_id: number
+  metricType: string
+  createdAt: string
+  data: {
+    os: { release: string; hostname: string; platform: string }
+    cpu: { cores: number; model: string; speed: number }
+    gpu: Array<{ vram: number; model: string }>
+    ram: { total: number }
+    disk: Array<{
+      fs: string
+      rw: boolean
+      use: number
+      size: number
+      type: string
+      used: number
+      mount: string
+      available: number
+    }>
+    network: Array<{
+      ip4: string
+      mac: string
+      iface: string
+    }>
+    identifier: string
+  }
+}
+
+export interface MachineDetailsProps {
+  machineId: number
+  onBack: () => void
+}
--- admin-page/src/firebase-config.ts
+++ admin-page/src/firebase-config.ts
@@ -1,16 +1,16 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC90lKBx6ZkgOCqiZ5bnixqoB3u0rkTfDU",
-  authDomain: "jbnu-capstone-2024.firebaseapp.com",
-  projectId: "jbnu-capstone-2024",
-  storageBucket: "jbnu-capstone-2024.appspot.com",
-  messagingSenderId: "922757487427",
-  appId: "1:922757487427:web:afb27312ceaa568a522df6"
-};
+  apiKey: 'AIzaSyC90lKBx6ZkgOCqiZ5bnixqoB3u0rkTfDU',
+  authDomain: 'jbnu-capstone-2024.firebaseapp.com',
+  projectId: 'jbnu-capstone-2024',
+  storageBucket: 'jbnu-capstone-2024.appspot.com',
+  messagingSenderId: '922757487427',
+  appId: '1:922757487427:web:afb27312ceaa568a522df6'
+}
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
 
-export { auth };
+export { auth }

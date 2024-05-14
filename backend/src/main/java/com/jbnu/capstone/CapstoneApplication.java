68d00562258668f7dc78536156782f2baea0a37e
iju42829 <ijud4282@gmail.com>
2024-05-14 15:38:49 +0900

    Scheduling을 위한 Annotation 추가

--- backend/src/main/java/com/jbnu/capstone/CapstoneApplication.java
+++ backend/src/main/java/com/jbnu/capstone/CapstoneApplication.java
@@ -3,7 +3,9 @@ package com.jbnu.capstone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class CapstoneApplication {
 

3f464d71f3b2ade3002196715b750fa9c1e7cf2d
iju42829 <ijud4282@gmail.com>
2024-05-15 21:30:59 +0900

    Daemon 연결을 위한 DTO

---
+++ backend/src/main/java/com/jbnu/capstone/dto/request/RequestMachineConnectionDTO.java
@@ -0,0 +1,13 @@
+package com.jbnu.capstone.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestMachineConnectionDTO {
+    private Long machineId;
+}

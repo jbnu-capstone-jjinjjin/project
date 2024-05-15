760bfdbb367df5ec3cd4d4619648c3850b988785
DeokJong <129056857+DeokJong@users.noreply.github.com>
2024-05-15 19:05:13 +0900

    프로젝트 변경 및 nwjs 트레이 작성 (#23)
    1. 기본 react 프로젝트에서 create-nw-react-app 으로 변경.
    2. dataHandler의 dataInterface 변경
    3. dataSender.ts 로직 변경
    4. dataCollector.ts 로직 변경
    5. App.tsx 로직 변경
    6. 로그인과 관련된 컴포넌트, 로직 제거
    7. 트레이 시스템 구현
    8. 백그라운드 실행 구현

--- collector/src/App.tsx
+++ collector/src/App.tsx
@@ -1,27 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import DataSection from "./components/DataSection";
+import { MetricType } from "./dataHandler/MetricTypes";
 
-import LoginPage from './components/LoginPage';
-import MainPage from './components/MainPage';
-import ProtectedRoute from './components/ProtectedRoute';
-import { auth } from './firebaseConfig';
-
-export default function App(): JSX.Element {
-  const [user, loading, error] = useAuthState(auth);
-  console.log("App: User:", user, "Loading:", loading, "Error:", error);
-
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={
-          <ProtectedRoute user={user} loading={loading} error={error}>
-            <MainPage />
-          </ProtectedRoute>
-        } />
-      </Routes>
-    </Router>
+    <header id='header' >
+      <DataSection metricType={MetricType.HW_INFO}/>
+      <DataSection metricType={MetricType.SDK_INFO}/>
+      <DataSection metricType={MetricType.RESOURCE_INFO}/>
+    </header>
   );
 }
 
-
+export default App;
---
+++ collector/src/components/DataSection.tsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+
+import {sendAndGetHwInfo,sendAndGetResourceInfo,sendAndGetSDKInfo,} from "../dataHandler/dataSender";
+import { MetricType } from "../dataHandler/MetricTypes";
+import { machineData } from "../dataHandler/dataInterface";
+
+type DataSectionProps = {
+  metricType: MetricType;
+};
+
+function initializeMachineData(metricType: MetricType): Promise<machineData> {
+  switch (metricType) {
+    case MetricType.HW_INFO:
+      return sendAndGetHwInfo();
+    case MetricType.RESOURCE_INFO:
+      return sendAndGetResourceInfo();
+    case MetricType.SDK_INFO:
+      return sendAndGetSDKInfo();
+    default:
+      return Promise.resolve({
+        info: null,
+        metricType: null,
+      });
+  }
+}
+
+function renderInfoContent(infoData: any) {
+  return Object.entries(infoData).map(([key, value]) => {
+    if (value && typeof value === "object" && !Array.isArray(value)) {
+      return (
+        <div key={key}>
+          <h3>{key}</h3>
+          {renderInfoContent(value)}
+        </div>
+      );
+    } else if (Array.isArray(value)) {
+      return (
+        <div key={key}>
+          <h3>{key}</h3>
+          {value.map((item, index) => (
+            <div key={index}>{renderInfoContent(item)}</div>
+          ))}
+        </div>
+      );
+    } else {
+      return <p key={key}>{`${key}: ${value ?? "Not available"}`}</p>;
+    }
+  });
+}
+
+function DataSection({ metricType }: DataSectionProps): React.ReactElement {
+  const [info, setInfo] = useState<machineData | null>(null);
+
+  useEffect(() => {
+    initializeMachineData(metricType).then((data) => {
+      setInfo(data);
+    });
+  }, [metricType]);
+
+  return (
+    <div id="result">
+      {info ? (
+        <div>
+          <h2>{`${info.metricType} Information:`}</h2>
+          {info.info ? renderInfoContent(info.info) : <p>No data available.</p>}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default DataSection;

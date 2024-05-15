ad465bf9648c0bf643325a5cd8b7a8a44e2bed81
iju42829 <ijud4282@gmail.com>
2024-05-15 21:40:56 +0900

    데몬 연결 및 명령 제어를 위한 DaemonController 구현

---
+++ backend/src/main/java/com/jbnu/capstone/controller/DaemonController.java
@@ -0,0 +1,31 @@
+package com.jbnu.capstone.controller;
+
+import com.jbnu.capstone.dto.request.RequestDaemonCommandDTO;
+import com.jbnu.capstone.dto.request.RequestMachineConnectionDTO;
+import com.jbnu.capstone.dto.response.ResponseDTO;
+import com.jbnu.capstone.service.SseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/daemon")
+public class DaemonController {
+    private final SseService sseService;
+
+    @PostMapping("/connect")
+    @ResponseStatus(HttpStatus.OK)
+    public SseEmitter connectDaemon(@RequestBody RequestMachineConnectionDTO requestMachineConnectionDTO) {
+        return sseService.createEmitter(requestMachineConnectionDTO.getMachineId());
+    }
+
+    @PostMapping("/control")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseDTO controlDaemon(@RequestBody RequestDaemonCommandDTO requestDaemonCommandDTO) {
+        sseService.sendToDaemon(requestDaemonCommandDTO.getMachineId(), requestDaemonCommandDTO.getControl());
+
+        return new ResponseDTO(HttpStatus.OK.value(), "명령 제어 전송에 성공했습니다.");
+    }
+}

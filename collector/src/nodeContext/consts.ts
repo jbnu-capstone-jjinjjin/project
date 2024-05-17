9027362d4963c44b2765ae1565672c245368083c
jungooji <zzzz465@naver.com>
2024-05-17 19:50:19 +0900

    add consts

--- collector/src/nodeContext/consts.ts
+++ collector/src/nodeContext/consts.ts
@@ -1,3 +1,8 @@
 import * as path from 'path'
 
-export const execDirPath = path.dirname(process.execPath)
+const nssmVersion = '2.24'
+const nssmDir = path.resolve(process.cwd(), 'nssm', nssmVersion)
+export const nssmPath = {
+  win32: path.resolve(nssmDir, 'win32', 'nssm.exe'),
+  win64: path.resolve(nssmDir, 'win64', 'nssm.exe'),
+}

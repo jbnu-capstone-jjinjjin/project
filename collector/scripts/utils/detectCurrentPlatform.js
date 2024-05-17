daf5da9143728f1ee0c73dd175b6f2551d6ab147
jungooji <zzzz465@naver.com>
2024-05-17 15:04:47 +0900

    add misisng utils code

---
+++ collector/scripts/utils/detectCurrentPlatform.js
@@ -0,0 +1,27 @@
+/* eslint-disable import/order */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+'use strict'
+
+/**
+ * detect the current platform according to expected constants
+ *
+ * return undefined if platform is unrecognized
+ */
+module.exports = function () {
+  switch (process.platform) {
+    case 'darwin':
+      return process.arch === 'x64' || process.arch === 'arm64'
+        ? 'osx64'
+        : 'osx32'
+
+    case 'win32':
+      return process.arch === 'x64' ||
+      process.env.PROCESSOR_ARCHITEW6432
+        ? 'win64'
+        : 'win32'
+
+    case 'linux':
+      return process.arch === 'x64' ? 'linux64' : 'linux32'
+  }
+}

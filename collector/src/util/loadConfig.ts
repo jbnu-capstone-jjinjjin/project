8df9db190e1d96675558d0a938bbeff2f99a445b
DeokJong <129056857+DeokJong@users.noreply.github.com>
2024-05-20 16:16:25 +0900

    feat : config 파일 읽어오기  (#32)
    1. 설정 파일에서 전송 주기를 읽고 이를 적용하는 것 구현

---
+++ collector/src/util/loadConfig.ts
@@ -0,0 +1,21 @@
+import * as path from 'path'
+import * as fs from 'fs'
+
+import { publicPath } from './consts'
+
+export interface Config {
+  collection_interval: number
+  pc_name: string
+}
+
+export function loadConfig() {
+  try {
+    const configFilePath = path.resolve(publicPath, 'config.json')
+    const data = fs.readFileSync(configFilePath, 'utf-8')
+    const config = JSON.parse(data)
+    return config
+  } catch (error: unknown) {
+    const err = error as Error
+    throw new Error(`Failed to read config file: ${err.message}`)
+  }
+}

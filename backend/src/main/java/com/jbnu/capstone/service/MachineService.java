c9a51ce823f5f68bdfc5cc94cd94fa92286c5d4d
iju42829 <ijud4282@gmail.com>
2024-05-24 15:10:32 +0900

    UUID로 머신 조회 기능 구현

--- backend/src/main/java/com/jbnu/capstone/service/MachineService.java
+++ backend/src/main/java/com/jbnu/capstone/service/MachineService.java
@@ -3,6 +3,7 @@ package com.jbnu.capstone.service;
 import com.jbnu.capstone.dto.request.RequestMachineDTO;
 import com.jbnu.capstone.dto.response.ResponseMachineDTO;
 import com.jbnu.capstone.entity.Machine;
+import com.jbnu.capstone.exception.MachineNotFoundException;
 import com.jbnu.capstone.repository.MachineRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.UUID;
 
 @Service
 @Transactional
@@ -21,11 +23,22 @@ public class MachineService {
     public ResponseMachineDTO createMachine(RequestMachineDTO machineDTO) {
         Machine machine = Machine.builder()
                 .machineName(machineDTO.getMachineName())
+                .uuid(machineDTO.getUuid())
                 .build();
 
         Machine savedMachine = machineRepository.save(machine);
 
         return new ResponseMachineDTO(savedMachine.getId(), savedMachine.getMachineName());
+    }
+
+    public ResponseMachineDTO findMachineIdByUuid(UUID uuid) throws MachineNotFoundException {
+        Machine machine = machineRepository.findByUuid(uuid);
+
+        if (machine == null) {
+            throw new MachineNotFoundException("uuid에 해당하는 머신을 찾을 수 없습니다.");
+        }
+
+        return new ResponseMachineDTO(machine.getId(), machine.getMachineName());
     }
 
     public void updateMachine(Long id, RequestMachineDTO machineDTO) {

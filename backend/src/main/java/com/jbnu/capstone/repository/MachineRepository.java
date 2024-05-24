e39da3bb9eece752329a3d284f32131d7eaef727
iju42829 <ijud4282@gmail.com>
2024-05-24 15:09:22 +0900

    MachineRepository에 findByUuid 메서드 추가

--- backend/src/main/java/com/jbnu/capstone/repository/MachineRepository.java
+++ backend/src/main/java/com/jbnu/capstone/repository/MachineRepository.java
@@ -4,7 +4,9 @@ import com.jbnu.capstone.entity.Machine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
 public interface MachineRepository extends JpaRepository<Machine, Long> {
-
+    Machine findByUuid(UUID uuid);
 }

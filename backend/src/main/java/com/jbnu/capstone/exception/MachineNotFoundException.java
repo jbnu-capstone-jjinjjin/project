d33fe8fdd217647498e8fbbe347763d6f1f62bfd
iju42829 <ijud4282@gmail.com>
2024-05-24 15:05:53 +0900

    MachineNotFoundException 예외 클래스 추가

---
+++ backend/src/main/java/com/jbnu/capstone/exception/MachineNotFoundException.java
@@ -0,0 +1,7 @@
+package com.jbnu.capstone.exception;
+
+public class MachineNotFoundException extends Exception {
+    public MachineNotFoundException(String message) {
+        super(message);
+    }
+}
